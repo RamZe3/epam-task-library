833ef733b33f7488e9f83ed4df6d7054bc195f55
Ramil_Fitkulin@epam.com <Ramil_Fitkulin@epam.com>
2021-08-14 03:44:28 +0400

    done Regex

---
+++ Epam.Library/Epam.Library.BLL/InformationResourceLogic.cs
@@ -0,0 +1,13 @@
+﻿using Epam.Library.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Library.BLL
+{
+    public class InformationResourceLogic : IInformationResourceLogic
+    {
+    }
+}
---
+++ Epam.Library/Epam.Library.RAMMemoryDAL/RAMMemory.cs
@@ -0,0 +1,37 @@
+﻿using Epam.Library.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epam.Library.RAMMemoryDAL
+{
+    public class RAMMemory : IInformationResourceDAL
+    {
+        public void AddBook(Library.Entities.Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPaper(Library.Entities.Paper paper)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPatent(Library.Entities.Patent patent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteResource(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Library.Entities.InformationResource> GetLibrary()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
